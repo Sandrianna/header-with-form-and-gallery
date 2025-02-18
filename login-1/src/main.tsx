0c3b7fa1b26550e772fdc4dac1d34701ab27bab4
Sandrianna <sashagoncharik6@gmail.com>
2025-02-18 18:25:47 +0300

    feat: add Router for the project

--- login-1/src/main.tsx
+++ login-1/src/main.tsx
@@ -1,11 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './main.css'
-import Home from './Home.jsx'
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router";
+import "./main.css";
+import Home from "./Home.jsx";
 
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
     <Home />
-  </StrictMode>,
-)
+  </BrowserRouter>
+);
