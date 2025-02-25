7f151b98e9263321fedc38a5a489b090b000812f
Sandrianna <sashagoncharik6@gmail.com>
2025-02-25 15:21:52 +0300

    Add GET to Profile

--- login-1/src/login-profile-page/Profile.jsx
+++ login-1/src/login-profile-page/Profile.jsx
@@ -1,13 +1,40 @@
 import { useNavigate } from "react-router";
-import { Button, Container, Typography, Paper } from "@mui/material";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import {
+  Button,
+  Container,
+  Typography,
+  Paper,
+  Box,
+  CircularProgress,
+  Alert,
+} from "@mui/material";
 
 export default function Profile({ message, setLogIn }) {
   const navigate = useNavigate();
+  const [profile, setProfile] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
 
   const handleLogOut = () => {
     setLogIn(false);
     navigate("/login");
   };
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/auth/profile")
+      .then((response) => {
+        setProfile(response.data);
+        setLoading(false);
+      })
+
+      .catch((err) => {
+        setError("Ошибка загрузки профиля");
+        setLoading(false);
+      });
+  }, []);
 
   return (
     <Container maxWidth="sm">
@@ -15,8 +42,19 @@ export default function Profile({ message, setLogIn }) {
         elevation={3}
         sx={{ padding: 4, textAlign: "center", marginTop: 9 }}
       >
-        <Typography variant="h4" gutterBottom>
-          Вход выполнен успешно!
+        {loading && (
+          <Box
+            display="flex"
+            justifyContent="center"
+            sx={{ marginTop: "20px" }}
+          >
+            <CircularProgress />
+          </Box>
+        )}
+        <Typography variant="h5" gutterBottom>
+          {profile?.username
+            ? `Добро пожаловать, ${profile.username}!`
+            : "Вход выполнен успешно, однако данные не прогрузились"}
         </Typography>
         <Typography variant="body1" color="textSecondary">
           {message}
@@ -30,6 +68,11 @@ export default function Profile({ message, setLogIn }) {
           Выйти
         </Button>
       </Paper>
+      {error && (
+        <Alert severity="error" sx={{ marginTop: 10 }}>
+          {error}
+        </Alert>
+      )}
     </Container>
   );
 }
